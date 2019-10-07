911896f9c88830519e97d4b7687c90413c7cd3b4
Jaehyun Lee <myleje@gmail.com>
2019-10-07 15:49:12 +0900

    .

---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    public Transform firePos;
+    public GameObject bulletPrefab;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Instantiate(생성할 프리팹, 위치, 각도)
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }        
+    }
+}
