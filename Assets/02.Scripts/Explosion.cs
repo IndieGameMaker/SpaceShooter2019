5b51c050ed811b4afbcd9d4d87d42e610d9861f1
Jaehyun Lee <myleje@gmail.com>
2019-10-08 09:47:16 +0900

    .

---
+++ Assets/02.Scripts/Explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public int hitCount = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        
+    }
+
+}
