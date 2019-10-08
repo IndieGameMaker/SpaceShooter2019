826ed9915638c1bb5a8fce79846319ab95c3f194
Jaehyun Lee <myleje@gmail.com>
2019-10-08 10:37:35 +0900

    .

--- Assets/02.Scripts/Explosion.cs
+++ Assets/02.Scripts/Explosion.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Explosion : MonoBehaviour
 {
     public int hitCount = 0;
+    public GameObject explosionEffect;
+    public AudioClip expSFX;
+
+    private AudioSource _audio;
 
     void Start()
     {
-        
+        _audio = GetComponent<AudioSource>();
     }
 
     void OnCollisionEnter(Collision coll)
@@ -23,8 +28,13 @@ public class Explosion : MonoBehaviour
         }
     }
 
-    void ExplosionBarrel()
+    public void ExplosionBarrel()
     {
+        GameObject effect = Instantiate(explosionEffect
+                                        , transform.position
+                                        , Quaternion.identity);
+        Destroy(effect, 1.5f);
+
         Collider[] colls = Physics.OverlapSphere(transform.position
                                                 ,10.0f
                                                 ,1<<8);
@@ -37,6 +47,16 @@ public class Explosion : MonoBehaviour
                rb = colls[i].gameObject.AddComponent<Rigidbody>();
             }
             rb.AddExplosionForce(1500.0f, transform.position, 10.0f, 1500.0f);
+            colls[i].GetComponent<Explosion>().ExplosionBarrelSelf();
         }
+    }
+
+    public void ExplosionBarrelSelf()
+    {
+        GameObject effect = Instantiate(explosionEffect
+                                        , transform.position
+                                        , Quaternion.identity);
+        Destroy(effect, 1.5f);
+        Destroy(this.gameObject);
     }
 }
