f5f3f5f316def9166a62c00b3cef4a7b5ffb1820
Jaehyun Lee <myleje@gmail.com>
2019-10-08 14:49:55 +0900

    .

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -15,12 +15,24 @@ public class MonsterCtrl : MonoBehaviour
     public State state = State.IDLE;
     public float attackDist = 2.0f;     //공격 사정거리
     public float traceDist  = 10.0f;    //추적 사정거리
+    public bool isDie = false;          //몬스터의 사망여부
 
+    private Transform playerTr;         //주인공의 위치 추출을 위한 Transform
+    private Transform monsterTr;        //몬스터의 위치 추출을 위한 Transform
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        playerTr = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Transform>();
+
+        GameObject monsterObj = GameObject.FindGameObjectWithTag("MONSTER");
+        if (monsterObj != null)
+        {
+            monsterTr = monsterObj.GetComponent<Transform>();
+        }
+        else
+        {
+            Debug.LogError("MONSTER Tag not found !!!");
+        }
     }
 
     // Update is called once per frame
