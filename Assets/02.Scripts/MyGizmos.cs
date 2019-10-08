e56afc6be45998413cd5e9fafdc6d4bf1e85d4a2
Jaehyun Lee <myleje@gmail.com>
2019-10-08 09:33:01 +0900

    .

---
+++ Assets/02.Scripts/MyGizmos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGizmos : MonoBehaviour
+{
+    public Color _color = Color.yellow;
+    [Range(0.1f, 0.8f)]
+    public float _radius = 0.3f;
+    public enum Type { SPHERE, WIRE }
+    public Type type = Type.SPHERE;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = _color;
+        if (type == Type.SPHERE)
+            Gizmos.DrawSphere(transform.position, _radius);
+        else if (type == Type.WIRE)
+            Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+}
