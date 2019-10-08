b1f75fee39787db1283f24cdbbeb1837b7b32ed4
Jaehyun Lee <myleje@gmail.com>
2019-10-08 09:59:49 +0900

    .

--- Assets/02.Scripts/Explosion.cs
+++ Assets/02.Scripts/Explosion.cs
@@ -13,7 +13,30 @@ public class Explosion : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
-        
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            if (++hitCount == 3)
+            {
+                ExplosionBarrel();
+                Destroy(this.gameObject);
+            }
+        }
     }
 
+    void ExplosionBarrel()
+    {
+        Collider[] colls = Physics.OverlapSphere(transform.position
+                                                ,10.0f
+                                                ,1<<8);
+
+        for(int i=0; i<colls.Length; i++)
+        {
+            Rigidbody rb = null;
+            if (colls[i].gameObject.GetComponent<Rigidbody>() == null)
+            {
+               rb = colls[i].gameObject.AddComponent<Rigidbody>();
+            }
+            rb.AddExplosionForce(1500.0f, transform.position, 10.0f, 1500.0f);
+        }
+    }
 }
